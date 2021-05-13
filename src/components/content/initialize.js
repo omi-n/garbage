b9e8bc93f19c130baab2a91eb46d4642a02a7a1b
Nabil <nabilomi1@gmail.com>
2021-05-13 13:31:04 -0400

    added init file

---
+++ src/components/content/initialize.js
@@ -0,0 +1,22 @@
+import firebase from "firebase/app";
+// This is just so I don't get my app messed with while it's on GitHub.
+// Firebase does not support .env for some reason? Send me a message on GitHub if this changed.
+const { 
+    apiKey, 
+    authDomain, 
+    projectId, 
+    storageBucket, 
+    messagingSenderId, 
+    appId
+  } = require("./firebaseInit.json");
+
+export default function Init() {
+    firebase.initializeApp({
+        apiKey: apiKey,
+        authDomain: authDomain,
+        projectId: projectId,
+        storageBucket: storageBucket,
+        messagingSenderId: messagingSenderId,
+        appId: appId
+    });
+}
